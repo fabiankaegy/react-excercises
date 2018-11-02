be89b4b8ad9f93a2adfe0a2f22f383d2df713d74
fabiankaegy <fabian@arvernus.info>
2018-11-02 15:15:17 +0100

    Updates

--- precache-manifest.03673fdd1d6c77246a63e7f6015dd80c.js
+++ precache-manifest.03673fdd1d6c77246a63e7f6015dd80c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-excercises/static/js/runtime~main.34e2b4ad.js"
   },
   {
-    "revision": "ab0793d5b050e163d51a",
-    "url": "/react-excercises/static/js/main.ab0793d5.chunk.js"
+    "revision": "c4104055ca1f51b94a65",
+    "url": "/react-excercises/static/js/main.c4104055.chunk.js"
   },
   {
     "revision": "6eb6405c9d99258624bc",
     "url": "/react-excercises/static/js/1.6eb6405c.chunk.js"
   },
   {
-    "revision": "ab0793d5b050e163d51a",
-    "url": "/react-excercises/static/css/main.dce76db9.chunk.css"
+    "revision": "c4104055ca1f51b94a65",
+    "url": "/react-excercises/static/css/main.5f0e982a.chunk.css"
   },
   {
-    "revision": "e64e53693a32cfe76fa5486f66527249",
+    "revision": "d137c5623e0949000ea788805b0b8de2",
     "url": "/react-excercises/index.html"
   }
 ];
