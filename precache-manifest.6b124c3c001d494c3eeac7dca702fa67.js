832622236fd8c367751e31a291c2c125f4d24efa
fabiankaegy <fabian@arvernus.info>
2018-11-02 09:29:38 +0100

    Updates

---
+++ precache-manifest.6b124c3c001d494c3eeac7dca702fa67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34e2b4ad687614158433",
+    "url": "/react-excercises/static/js/runtime~main.34e2b4ad.js"
+  },
+  {
+    "revision": "34ed632e8c380336232f",
+    "url": "/react-excercises/static/js/main.34ed632e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-excercises/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "34ed632e8c380336232f",
+    "url": "/react-excercises/static/css/main.cbf43c09.chunk.css"
+  },
+  {
+    "revision": "7376a15afcad6dc9e2f3b0555bf89818",
+    "url": "/react-excercises/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b1886db5c25f2a97505418c279eb945.js"
+  "/react-excercises/precache-manifest.6b124c3c001d494c3eeac7dca702fa67.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-excercises/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
